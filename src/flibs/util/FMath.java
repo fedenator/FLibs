ce51a766d19b6bdf85407276d30aa25e2b9d30aa
FPalacios <fedenator7@gmail.com>
2016-11-18 00:27:39 -0300

    Cambio en la estructura como libreria 3

--- src/flibs/util/FMath.java
+++
@@ -1,7 +0,0 @@
-package flibs.util;
-
-public class FMath {
-	public static double getPortentage(double num, double porcentage) {
-		return (num/100) * porcentage;
-	}
-}
--- src/flibs/util/StringUtilities.java
+++
@@ -1,7 +0,0 @@
-package flibs.util;
-
-public class StringUtilities {
-	public static String removeLastCharacter(String str) {
-		return str.substring(0, str.length() - 1);
-	}
-}
