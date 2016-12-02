515eab269a8586d87af9a5c71b2982427e7f1221
Federico Palacios <fedenator7@gmail.com>
2016-12-02 17:52:13 -0300

    afuera la posicion relativa del styledata

--- flibs/util/StyleData.java
+++ flibs/util/StyleData.java
@@ -7,28 +7,22 @@ package flibs.util;
  */
 public class StyleData {
 	public static final byte UNIT_PIXELS=0, UNIT_PERCENTAGE=1;
-	public static final byte POSITION_RELATIVE=0, POSITION_ABSOLUTE=1;
 	
 	public byte unit;
-	public byte position;
 	public int value;
 	
-	public StyleData(byte unit, byte position, int value) {
+	public StyleData(byte unit, int value) {
 		this.unit = unit;
-		this.position = position;
 		this.value = value;
 	}
 	
-	public StyleData(byte unit, int value) {
-		this(unit, POSITION_RELATIVE, value);
-	}
 	
 	public StyleData(int value) {
-		this(UNIT_PIXELS, POSITION_RELATIVE, value);
+		this(UNIT_PIXELS, value);
 	}
 	
 	public StyleData() {
-		this(UNIT_PIXELS, POSITION_RELATIVE, 0);
+		this(UNIT_PIXELS, 0);
 	}
 	
 	@Override
